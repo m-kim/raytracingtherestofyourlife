68809da4720c8a376da19682e234855d617f7cce
Mark Kim <mbk@cs.utah.edu>
2019-06-03 15:12:50 -0400

    why are these having linking issues?

--- pathtracing/SpherePdf.h
+++ pathtracing/SpherePdf.h
@@ -3,6 +3,9 @@
 #include "Pdf.h"
 #include <vtkm/cont/ArrayHandle.h>
 #include <vtkm/rendering/raytracing/Ray.h>
+#include "PdfWorklet.h"
+#include <vtkm/worklet/Invoker.h>
+
 class SpherePdf : public Pdf
 {
 public:
@@ -16,7 +19,35 @@ public:
   vtkm::cont::ArrayHandle<vtkm::Id>  SphereIds, light_pointids;
   vtkm::cont::ArrayHandle<vtkm::Float32>  SphereRadii;
 
-  void apply(vtkm::rendering::raytracing::Ray<vtkm::Float32> &rays);
+  void apply(vtkm::rendering::raytracing::Ray<vtkm::Float32> &rays)
+  {
+    vtkm::worklet::Invoker Invoke;
+    SpherePDFWorklet spherePDFWorklet(lightables);
+    SphereExecWrapper surf(SphereIds, SphereRadii, MatIdx, TexIdx);
+    using vec3CompositeType = vtkm::cont::ArrayHandleCompositeVector<
+      vtkm::cont::ArrayHandle<vtkm::Float32>,
+      vtkm::cont::ArrayHandle<vtkm::Float32>,
+      vtkm::cont::ArrayHandle<vtkm::Float32>>;
+    auto generated_dir = vec3CompositeType(
+          rays.GetBuffer("generated_dirX").Buffer,rays.GetBuffer("generated_dirY").Buffer,rays.GetBuffer("generated_dirZ").Buffer);
+
+    auto sum_values = rays.GetBuffer("sum_values").Buffer;
+    auto hrecs = HitRecord(rays.U, rays.V, rays.Distance, rays.NormalX, rays.NormalY, rays.NormalZ, rays.IntersectionX, rays.IntersectionY, rays.IntersectionZ);
+
+
+    Invoke(spherePDFWorklet,
+           rays.Origin,
+           rays.Dir,hrecs,
+           rays.Status,
+           sum_values,
+           generated_dir,
+           seeds,
+           surf,
+           light_pointids,
+           light_indices,
+           coordsHandle,
+           SphereRadii);
+  }
 
 };
 
